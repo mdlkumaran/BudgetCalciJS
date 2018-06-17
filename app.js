aed4dffacc8931150486f3f7b3e1800518a2e8d0
Kumaran MDL <mdlkumaran@gmail.com>
2018-06-16 22:37:03 -0700

    adding resources and adding event listeners

---
+++ app.js
@@ -0,0 +1,38 @@
+//Budget Controller
+var budgetController = (function () {
+    //Some code
+})();
+
+//UI Controller
+var UIController = (function () {
+    //Some code
+})();
+
+//Global App Controller
+var Controller = (function (budgetCtrl, UICtrl) {
+
+    var ctrlAddItem = function () {
+        //1. Get the field input data
+
+        //2. Add the item to the budget controller
+
+        //3. Add the new item to the UI
+
+        //4. Calculate the budget
+
+        //5. Display the budget on the UI
+
+        console.log('ctrlAddItem works')
+    };
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function (event) {
+
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+
+    });
+
+})(budgetController, UIController);
